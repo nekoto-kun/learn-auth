acb829de3348df87152348925845063294e7062f
Andre Kho <andreyonathan65@gmail.com>
2021-11-12 10:15:05 +0700

    Make separate access to each view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,13 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        switch (Auth::user()->role) {
+            case 0:
+                return view('customer.home');
+            case 1:
+                return view('admin.home');
+            case 2:
+                return view('supervisor.home');
+        }
     }
 }
